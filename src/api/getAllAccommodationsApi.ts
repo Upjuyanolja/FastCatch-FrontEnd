6031e6b9b9059fa420462d96eed26db9b55194ae
89882 <pgh123166@naver.com>
2023-12-26 23:15:17 +0900

    Feat: 메인 페이지 infinite query 구현

---
+++ src/api/getAllAccommodationsApi.ts
@@ -0,0 +1,44 @@
+import instance from "./instanceApi";
+
+export type AccommodationParams = {
+  category: string;
+  page: number;
+  hasCoupon: boolean;
+};
+export type Accommodation = {
+  id: number;
+  name: string;
+  address: string;
+  category: string;
+  lowestPrice: number;
+  discountPrice: number;
+  imageUrl: string;
+  soldOut: boolean;
+  coupon: string;
+};
+export type Accommodations = {
+  pageNum: number;
+  pageSize: number;
+  totalPages: number;
+  totalElements: number;
+  isLast: boolean;
+  accommodations: Accommodation[];
+};
+export type ResponseAccommodation = {
+  message: string;
+  data: Accommodations;
+};
+
+export const getAllAccommodations = ({
+  category = "string",
+  hasCoupon = false,
+  page = 1,
+}: AccommodationParams) => {
+  return instance.get<ResponseAccommodation>("/api/accommodations", {
+    params: {
+      category,
+      hasCoupon,
+      page,
+    },
+  });
+};
---
+++ src/hooks/quries/useMain.ts
@@ -0,0 +1,31 @@
+import {
+  ResponseAccommodation,
+  getAllAccommodations,
+} from "@/api/getAllAccommodationsApi";
+import { AxiosError, AxiosResponse } from "axios";
+import { useInfiniteQuery } from "react-query";
+
+export const useGetAllAccommodations = (
+  category: string,
+  hasCoupon: boolean
+) => {
+  return useInfiniteQuery<
+    AxiosResponse<ResponseAccommodation>,
+    AxiosError,
+    AxiosResponse<ResponseAccommodation>
+  >(
+    ["accommodations-list"],
+    ({ pageParam = 1 }) =>
+      getAllAccommodations({ page: pageParam, category, hasCoupon }),
+    {
+      getNextPageParam: ({
+        data: {
+          data: { pageNum, totalPages },
+        },
+      }) => {
+        const nextPage = pageNum + 1;
+        return totalPages > pageNum ? nextPage : undefined;
+      },
+    }
+  );
+};
