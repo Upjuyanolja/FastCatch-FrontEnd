1539c10cf8b90407a113fd2842c360ebefbf884f
Diana Seo <syb0127@berkeley.edu>
2023-12-27 18:37:41 +0900

    Style: 드롭박스 스타일링 완료

--- src/pages/discount/Discount.tsx
+++ src/pages/discount/Discount.tsx
@@ -2,7 +2,7 @@ import { SetStateAction, memo, useState } from "react";
 
 //import DiscountItem from "@/pages/discount/discountItem/DiscountItem";
 import { discount } from "@/constant/discount";
-
+import { FaSortDown } from "react-icons/fa6";
 import "./discount.scss";
 
 type OptionType = {
@@ -25,19 +25,36 @@ const Discount = memo(() => {
     setIsOpen(false);
   };
 
+  const defaultOption: OptionType = { label: "선택없음", value: "" };
+
   return (
     <div className="discount">
       <h4 className="text-subtitle4">할인</h4>
       <div className="dropdown-container">
-        <div className="dropdown-header" onClick={toggleDropdown}>
-          {selectedOption ? selectedOption.label : "Select an option"}
-          <span className={`arrow ${isOpen ? "open" : ""}`}></span>
+        <div className="selected-option" onClick={toggleDropdown}>
+          <span className="label">
+            {selectedOption ? selectedOption.label : "선택안함"}
+          </span>
+          <span
+            className={`arrow ${isOpen ? "open" : ""}`}
+            onClick={toggleDropdown}
+          >
+            <FaSortDown />
+          </span>
         </div>
+
         {isOpen && (
           <ul className="dropdown-list">
-            {discount.map((option, index) => (
+            <li
+              key="default-option"
+              className="dropdown-item"
+              onClick={() => selectOption(defaultOption)}
+            >
+              {defaultOption.label}
+            </li>
+            {discount.map(option => (
               <li
-                key={index}
+                key={option.value}
                 className="dropdown-item"
                 onClick={() => selectOption(option)}
               >
