b3b6f9d55e15d157b3a074036b62a7f3514a2f18
xxxjinn <tlsguswls319@naver.com>
2023-12-28 18:02:40 +0900

    Chore: headers 삭제

--- src/api/deleteCancelReservation.ts
+++ src/api/deleteCancelReservation.ts
@@ -5,11 +5,5 @@ export const deleteCancelReservation = ({
 }: {
   reservationId: number;
 }) => {
-  const accessToken = localStorage.getItem("accessToken");
-  return instance.delete(`/api/reservations/${reservationId}`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      "Content-Type": "application/json",
-    },
-  });
+  return instance.delete(`/api/reservations/${reservationId}`);
 };
--- src/api/getReservationListApi.ts
+++ src/api/getReservationListApi.ts
@@ -33,12 +33,7 @@ export type ResponseReservation = {
 };
 
 export const getReservationList = ({ page = 1 }) => {
-  const accessToken = localStorage.getItem("accessToken");
   return instance.get<ResponseReservation>("/api/reservations", {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      "Content-Type": "application/json",
-    },
     params: {
       page,
     },
