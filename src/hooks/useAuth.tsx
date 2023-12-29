42359d85fcbb55d1ab6d9b5f54cd4c5246280945
yangjaehyuk <gndksakxk10@naver.com>
2023-12-29 14:33:08 +0900

    Fix: 상세페이지 이미지 안 불러오는 에러 해결

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -37,7 +37,7 @@ export async function refreshAccessToken() {
     },
   });
 
-  console.log("토큰 재발급 성공", response.data);
+  // console.log("토큰 재발급 성공", response.data);
   const newAccessToken = response.data.data.accessToken;
   localStorage.setItem("accessToken", newAccessToken);
 
