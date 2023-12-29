23416a5a0585465509509974769265f4f149a34b
Taegwan Youn <tkyoun0421@naver.com>
2023-12-29 22:04:02 +0900

    Feat: 쿠폰 드랍 박스 선택 안함 선택시 선택 안함 선택 아이템 삭제

--- src/pages/order/discount/Discount.tsx
+++ src/pages/order/discount/Discount.tsx
@@ -1,9 +1,9 @@
-import { memo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { FaSortDown } from "react-icons/fa6";
-import "./discount.scss";
-import { useRecoilValue, useRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { orderState } from "@/states/orderState";
 import { discountState } from "@/states/discountState";
+import "./discount.scss";
 
 const Discount = memo(() => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,19 +14,38 @@ const Discount = memo(() => {
     price: number;
   };
 
+  const [couponList, setCouponList] = useState<CouponType[] | []>([]);
   const [selectedCoupon, setSelectedCoupon] = useState<CouponType | null>(null);
-  const [discountAmt, setDiscountAmt] = useRecoilState(discountState);
+  const setDiscountAmt = useSetRecoilState(discountState);
   const toggleDropdown = () => setIsOpen(!isOpen);
 
   const selectCoupon = (coupon: CouponType) => {
     setSelectedCoupon(coupon);
     setDiscountAmt(coupon.price);
     setIsOpen(false);
+
+    if (
+      coupon.name === "선택안함" ||
+      !couponList.find(item => item.name === defaultOption.name)
+    ) {
+      setCouponList(
+        coupon.name === "선택안함"
+          ? order[0]?.coupons || []
+          : [defaultOption, ...couponList]
+      );
+    }
   };
 
   const defaultOption: CouponType = { name: "선택안함", id: 0, price: 0 };
 
   const order = useRecoilValue(orderState);
+
+  useEffect(() => {
+    if (!order) {
+      return;
+    }
+    setCouponList(order[0].coupons);
+  }, [order]);
 
   return (
     <div className="discount">
@@ -34,7 +53,6 @@ const Discount = memo(() => {
       <div className={`dropdown-container ${isOpen && "open"}`}>
         <div className="selected-option" onClick={toggleDropdown}>
           <span className="label">
-            {/* {selectedCoupon ? selectedCoupon.name : "선택안함"} */}
             {selectedCoupon ? selectedCoupon.name : "선택안함"}
           </span>
           <span
@@ -47,15 +65,7 @@ const Discount = memo(() => {
 
         {isOpen && (
           <ul className="dropdown-list">
-            <li
-              key="default-option"
-              className="dropdown-item"
-              onClick={() => selectCoupon(defaultOption)}
-            >
-              {defaultOption.name}
-            </li>
-
-            {order[0]?.coupons.map(coupon => (
+            {couponList.map(coupon => (
               <li
                 key={coupon.id}
                 className="dropdown-item"
